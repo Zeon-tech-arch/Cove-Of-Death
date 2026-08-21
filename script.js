0aa44f5ee4ea045977ea64db79f363d1efe11fd3
Zeon-tech-arch <yusufhanad250@gmail.com>
2026-08-21 16:28:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let correctUsername = "Zeon";
+let correctPassword = "169661?!";
 
+function login() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === correctUsername && password === correctPassword) {
+
+        window.location.href = "home.html";
+
+    } else {
+
+        document.getElementById("result").textContent =
+            "Incorrect username or password.";
+
+    }
+}
